522210d0b8dd88997f492dd4a0ab79c9eddf0fff
Víctor González Prieto <ar-victor.gonzalez@rakuten.com>
2019-10-20 01:25:24 +0200

    Create a card list of my repositories

--- client/index.js
+++ client/index.js
@@ -15,7 +15,19 @@ fragment commitFragment on Repository {
 }
 `;
 
-let queryRepoList;
+const queryRepoList = `
+{
+  viewer {
+    name
+    repositories (first: 12) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}
+`;
 
 let mutationAddStar;
 
@@ -50,7 +62,16 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest("{viewer { name } }", {}, (data) => {
+  gqlRequest(queryRepoList, {}, (data) => {
     $('header h2').text(`Hello ${data.viewer.name}`);
+    console.log(data);
+    const repos = data.viewer.repositories;
+    if (repos.totalCount > 0) {
+      $('ul.repos').empty();
+      repos.nodes.forEach((repo) => {
+        const card = `<li><h3>${repo.name}</h3></li>`;
+        $('ul.repos').append(card);
+      })
+    }
   });
 });
