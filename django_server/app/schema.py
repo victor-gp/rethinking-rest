3eee23c543994c2cd02d93c2e5fa670ac1f19af7
Víctor González Prieto <ar-victor.gonzalez@rakuten.com>
2019-10-20 14:47:06 +0200

    Limit scope of "users" query
    With only_fields we only allow the query to access the 'id' and
    'username' attributes of the UserModel. We also add a custom resolver
    for isAdmin.
    
    Notice the automatic transformation of snake_case in the backend
    (is_admin) to camelCase in the frontend (isAdmin). I guess the magic
    comes from Graphene?

--- django_server/app/schema.py
+++ django_server/app/schema.py
@@ -3,8 +3,14 @@ import graphene_django
 from django.contrib.auth.backends import UserModel
 
 class UserType(graphene_django.DjangoObjectType):
+    is_admin = graphene.Boolean()
+
+    def resolve_is_admin(self, info):
+        return self.is_staff
+
     class Meta:
         model = UserModel
+        only_fields = ('id', 'username')
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
