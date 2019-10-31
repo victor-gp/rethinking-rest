ad388073bf693a009b90c8eff69338caee449178
Víctor González Prieto <ar-victor.gonzalez@rakuten.com>
2019-10-31 07:49:44 +0100

    Add alternative keys to queries "user" and "book"
    The introduced keys are "username" and "title" respectively.
    
    I've also added a validation for "either"-type arguments, where one of
    two args is required, but both are not allowed.

--- django_server/app/schema.py
+++ django_server/app/schema.py
@@ -2,6 +2,7 @@ import graphene
 import graphene_django
 from django.contrib.auth.backends import UserModel
 from .models import Book, HasRead, read_book, average_rating
+from graphql import GraphQLError
 
 class UserType(graphene_django.DjangoObjectType):
     is_admin = graphene.Boolean()
@@ -26,12 +27,22 @@ class HasReadType(graphene_django.DjangoObjectType):
     class Meta:
         model = HasRead
 
+user_args = {
+    'id': graphene.Int(),
+    'username': graphene.String()
+}
+
+book_args = {
+    'id': graphene.Int(),
+    'title': graphene.String()
+}
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
-    user = graphene.Field(UserType, id=graphene.Int(required=True))
+    user = graphene.Field(UserType, **user_args)
     books = graphene.List(BookType, fiction=graphene.Boolean(),
         first=graphene.Int(), last=graphene.Int(), offset=graphene.Int())
-    book = graphene.Field(BookType, id=graphene.Int(required=True))
+    book = graphene.Field(BookType, **book_args)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
@@ -64,10 +75,29 @@ class Query(graphene.ObjectType):
         return q[start:end]
 
     def resolve_user(self, info, **kwargs):
-        return UserModel.objects.get(id=kwargs['id'])
+        Query.validate_either_args('id', 'username', kwargs)
+        if 'id' in kwargs:
+            return UserModel.objects.get(id=kwargs['id'])
+        elif 'username' in kwargs:
+            return UserModel.objects.get(username=kwargs['username'])
 
     def resolve_book(self, info, **kwargs):
-        return Book.objects.get(id=kwargs['id'])
+        Query.validate_either_args('id', 'title', kwargs)
+        if 'id' in kwargs:
+            return Book.objects.get(id=kwargs['id'])
+        elif 'title' in kwargs:
+            return Book.objects.get(title=kwargs['title'])
+
+    def validate_either_args(arg1, arg2, kwargs):
+        both_error_message = (f"Only one of arguments \"{arg1}\" and \"{arg2}\""
+                               " is allowed but both provided.")
+        neither_error_message = (f"One of arguments \"{arg1}\" and \"{arg2}\""
+                                  "is required but not provided.")
+        if arg1 in kwargs:
+            if arg2 in kwargs:
+                raise GraphQLError(both_error_message)
+        elif arg2 not in kwargs:
+            raise GraphQLError(neither_error_message)
 
 class ReadBook(graphene.Mutation):
     class Arguments:
